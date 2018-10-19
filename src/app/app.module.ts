d54c6c9126248bfdd717d20c11959572841addcb
Robert Todea <robert.todea@e-spres-oh.com>
2018-10-19 12:13:19 +0300

    Make some routes to show state is persisted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BoxComponent } from './box/box.component';
 import { ControllerComponent } from './controller/controller.component';
+import { AboutComponent } from './about/about.component';
+import { MainComponent } from './main/main.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     BoxComponent,
-    ControllerComponent
+    ControllerComponent,
+    AboutComponent,
+    MainComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '', component: MainComponent},
+      {path: 'about', component: AboutComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
